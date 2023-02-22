f45aa4dbd063560b013578cc06ce445639afbbaf
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-02-22 18:45:20 +0100

    Remove Indeks 2019 - Som løsningen, men sjekker ikke om det er raskere å traversere fra tail

---
+++ kode/src/host23/eksamen/E2019.java
@@ -0,0 +1,47 @@
+/*
+package host23.eksamen;
+
+public class E2019 {
+    public static class DoubleLinkedList {
+        public class Node {
+            Node next;
+            Node prev;
+            char value;
+        }
+        Node head;
+        Node tail;
+
+        void addLast(char value) {...}
+        void addFirst(char value) {...}
+        char removeLast() {...}
+        char removeFirst() {...}
+        void print() {...}
+
+        void remove(int index) {
+            if (index == 0) {
+                removeFirst();
+            } else if (index == size - 1) {
+                removeLast();
+            } else {
+                Node q = new Node(head.value);
+                for (int i = 0; i < index; i++) {
+                    q = q.next;
+                }
+                Node p = new Node(q.prev);
+                Node r = new Node(q.next);
+
+                p.next = r;
+                r.prev = q;
+
+                size--;
+            }
+        }
+        void remove(char value) {
+            // Din kode her
+        }
+    }
+}
+
+
+
+*/
