938a18170d858400b0360b9ee460a9128ace7c23
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-02-23 11:04:12 +0100

    CircularDequeExam1 - skall + constructor

---
+++ kode/src/host23/eksamen/CircularDequeExam1.java
@@ -0,0 +1,40 @@
+package host23.eksamen;
+
+import java.util.Deque;
+
+public class CircularDequeExam1 {
+    int begin; //< Starten av køen
+    int end; //< slutten av køen
+    int capacity; //< maksimal størrelse på køen
+    int size; //< antall elementer i køen så langt
+    char[] values;
+    CircularDequeExam1(int capacity) {
+        this.begin = 0;
+        this.end = 0;
+        this.capacity = capacity;
+        this.size = 0;
+        this.values = new char[capacity];
+    }
+    void pushBack(char value) {
+
+    }
+    void pushFront(char value) {
+        /**
+         * Denne må du implementere
+         */
+        throw new UnsupportedOperationException("Not implemented");
+    }
+    char popFront() {
+        /**
+         * Denne må du implementere
+         */
+        throw new UnsupportedOperationException("Not implemented");
+    }
+    char popBack() {
+        /**
+         * Denne må du implementere
+         */
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
+
