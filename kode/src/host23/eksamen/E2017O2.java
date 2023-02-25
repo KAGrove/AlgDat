fb1814f9e62864e95b18a3dfb42f70a5df5e269d
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-02-25 08:16:40 +0100

    Eksamen 2017 - Iterator - Med CG-main, uten interface-oppsett

--- kode/src/host23/eksamen/E2017O2.java
+++ kode/src/host23/eksamen/E2017O2.java
@@ -1,25 +1,30 @@
 package host23.eksamen;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class E2017O2 {
-    public interface Iterable<T>
-    {
-        Iterator<T> iterator();
 
-        public static <T> T maks(Iterable<T> s, Comparator<? super T> c){
-            Iterator<T> i = s.iterator();
-            if(!i.hasNext()) throw new NoSuchElementException("s er tom!");
+    public static <T> T maks(Iterable<T> s, Comparator<? super T> c) {
+        Iterator<T> i = s.iterator();
+        if (!i.hasNext()) throw new NoSuchElementException("s er tom!");
 
-            T maks = i.next();
+        T maks = i.next();
 
-            while (i.hasNext()){
-                T verdi = i.next();
-                if(c.compare(verdi, maks) > 0) maks = verdi;
-            }
-            return maks;
+        while (i.hasNext()) {
+            T verdi = i.next();
+            if (c.compare(verdi, maks) > 0) maks = verdi;
         }
+
+        return maks;
+    }
+
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>(Arrays.asList("apple", "banana", "orange", "kiwi"));
+        String maxString = maks(stringList, Comparator.naturalOrder());
+        System.out.println("Max string: " + maxString);
+
+        List<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        Integer maxInt = maks(intList, Comparator.reverseOrder());
+        System.out.println("Max integer: " + maxInt);
     }
 }
